37a3ead6851d385ea1fc1fea179e7a0d207e97ef
Natanael S <164245059+iLe4n@users.noreply.github.com>
2024-05-27 16:30:29 -0300

    Exercicio_2_Notas.py

--- Exercicio_2_Notas.py
+++ Exercicio_2_Notas.py
@@ -1,3 +1,8 @@
+#Implementar uma solução em Python que resolva a seguinte questão: 
+#Se a nota for maior ou igual a 7, o estudante foi aprovado. 
+#Se a nota for menor que 7 e maior ou igual a 5, o estudante está em recuperação. 
+#Se a nota for menor que 5, o estudante está reprovado. 
+
 media = 8.5 
 
 if (media>=7.0): 
