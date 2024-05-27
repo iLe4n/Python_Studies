910c3df96138e499bba3bf3c6a93bc343fdedd7f
Natanael S <164245059+iLe4n@users.noreply.github.com>
2024-05-27 16:31:50 -0300

    Exercicio_3_ValorCompra.py

--- Exercicio_3_ValorCompra.py
+++ Exercicio_3_ValorCompra.py
@@ -1,4 +1,12 @@
 
+#Implementar uma solução em Python que resolva a seguinte questão: 
+
+#Calcular o valor de uma compra, sendo que o preço unitário é de R$10,00
+#Se for feita uma compra de até 10 unidades, não há descontos. 
+#Para compras entre 11 e 20 unidades é dado um desconto de 10%. 
+#Acima de 20 unidades, é dado um desconto de 20%. 
+
+
 ComprasAoTodo = int(input(f'Por favor, digite qual foi a quantidade de produtos comprados: '))
 
 if (ComprasAoTodo <= 10): 
