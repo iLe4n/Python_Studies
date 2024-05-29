0e4f5c0f9bca89c1de278f9afd55b336baf3ed64
Natanael S <164245059+iLe4n@users.noreply.github.com>
2024-05-28 22:42:20 -0300

    Add files via upload

---
+++ Password_While_Exemple.py
@@ -0,0 +1,5 @@
+password = int(input (f'Digite a sua senha, por favor: \n'))
+while password != "123": 
+        password = input (f'Por favor digite a senha para logar:\n')
+   
+print(f'Parabéns, você acertou a senha! Bem vindo amigo!!')
